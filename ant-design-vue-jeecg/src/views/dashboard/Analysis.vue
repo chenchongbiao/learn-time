c3d8f0f3804d309a9ff2a8ec3fa21dc724cb4ecb
bluesky <379865549@qq.com>
2022-03-03 13:24:29 +0800

    修改首页样式显示的条件

--- ant-design-vue-jeecg/src/views/dashboard/Analysis.vue
+++ ant-design-vue-jeecg/src/views/dashboard/Analysis.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <index-chart v-if="indexStyle==1"></index-chart>
-    <index-bdc v-if="indexStyle==2"></index-bdc>
-    <index-task v-if="indexStyle==3"></index-task>
-    <div style="width: 100%;text-align: right;margin-top: 20px">
+    <index-chart v-if="userIdentity==1"></index-chart>
+    <index-bdc v-if="indexStyle==1 && userIdentity==2"></index-bdc>
+    <index-task v-if="indexStyle==2 && userIdentity==2"></index-task>
+    <div style="width: 100%;text-align: right;margin-top: 20px" v-if="userIdentity==2">
       请选择首页样式：
       <a-radio-group v-model="indexStyle">
+<!--        <a-radio :value="1" v-if="userIdentity==1">统计图表</a-radio>-->
         <a-radio :value="1">统计图表</a-radio>
-        <a-radio :value="2">统计图表2</a-radio>
-        <a-radio :value="3">任务表格</a-radio>
+        <a-radio :value="2">任务表格</a-radio>
       </a-radio-group>
     </div>
   </div>
@@ -18,7 +18,9 @@
   import IndexChart from './IndexChart'
   import IndexTask from "./IndexTask"
   import IndexBdc from './IndexBdc'
-
+  import Vue from 'vue'
+  import { USER_INFO } from "@/store/mutation-types"
+  import store from '@/store/'
   export default {
     name: "Analysis",
     components: {
@@ -28,14 +30,21 @@
     },
     data() {
       return {
-        indexStyle:1
+        indexStyle:1,
+        userIdentity: 1
       }
     },
     created() {
-
+      this.getIndex()
     },
     methods: {
-
+      // 根据不同用户选择显示不同的首页
+      getIndex(){
+        this.userIdentity = store.getters.userIdentity
+        console.log(Vue.ls)
+        console.log("userinfo",Vue.ls.get(USER_INFO))
+        console.log("indexStyle",this.indexStyle)
+      }
     }
   }
 </script>
