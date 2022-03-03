dc1b33c18cb7e1c1ea0dbdcd18eb7e92b2e556bc
bluesky <379865549@qq.com>
2022-03-03 15:00:18 +0800

    修改用户实体类

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/entity/SysUser.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/entity/SysUser.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jeecg.common.aspect.annotation.Dict;
 import org.jeecgframework.poi.excel.annotation.Excel;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -67,13 +66,60 @@ public class SysUser implements Serializable {
     /**
      * 头像
      */
-    @Excel(name = "头像", width = 15,type = 2)
+//    @Excel(name = "头像", width = 15,type = 2)
     private String avatar;
+
+    /**
+     * 年级
+     */
+    @Excel(name = "年级", width = 15)
+    private Integer grade;
+
+    /**
+     * 班级
+     */
+    @Excel(name = "班级", width = 15)
+    private String clazz;
+
+    /**
+     * 总学时
+     */
+    private Double total;
+
+    /**
+     * 创新创业素质学时
+     */
+//    @Excel(name = "创新创业素质", width = 15)
+    private Double innovation;
+
+    /**
+     * 思想品德素质学时
+     */
+//    @Excel(name = "思想品德素质", width = 15)
+    private Double thought;
+
+    /**
+     * 身心素质学时
+     */
+//    @Excel(name = "身心素质", width = 15)
+    private Double bodyMind;
+
+    /**
+     * 法律素养学时
+     */
+//    @Excel(name = "法律素养", width = 15)
+    private Double law;
+
+    /**
+     * 文体素质学时
+     */
+//    @Excel(name = "文体素质", width = 15)
+    private Double cultureSports;
 
     /**
      * 生日
      */
-    @Excel(name = "生日", width = 15, format = "yyyy-MM-dd")
+//    @Excel(name = "生日", width = 15, format = "yyyy-MM-dd")
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
@@ -81,61 +127,65 @@ public class SysUser implements Serializable {
     /**
      * 性别（1：男 2：女）
      */
-    @Excel(name = "性别", width = 15,dicCode="sex")
+//    @Excel(name = "性别", width = 15,dicCode="sex")
     @Dict(dicCode = "sex")
     private Integer sex;
 
     /**
      * 电子邮件
      */
-    @Excel(name = "电子邮件", width = 15)
+//    @Excel(name = "电子邮件", width = 15)
     private String email;
 
     /**
      * 电话
      */
-    @Excel(name = "电话", width = 15)
+//    @Excel(name = "电话", width = 15)
     private String phone;
 
     /**
      * 部门code(当前选择登录部门)
      */
+//    @Excel(name = "登录部门", width = 15)
     private String orgCode;
 
     /**部门名称*/
-    private transient String orgCodeTxt;
+    // orgCodeTxt 不会被序列化
+//    private transient String orgCodeTxt;
+    @Excel(name = "所在学院", width = 15)
+    private String orgCodeTxt;
 
     /**
      * 状态(1：正常  2：冻结 ）
      */
-    @Excel(name = "状态", width = 15,dicCode="user_status")
+//    @Excel(name = "状态", width = 15,dicCode="user_status")
     @Dict(dicCode = "user_status")
     private Integer status;
 
     /**
      * 删除状态（0，正常，1已删除）
      */
-    @Excel(name = "删除状态", width = 15,dicCode="del_flag")
+//    @Excel(name = "删除状态", width = 15,dicCode="del_flag")
     @TableLogic
     private Integer delFlag;
 
     /**
      * 工号，唯一键
      */
-    @Excel(name = "工号", width = 15)
+//    @Excel(name = "工号", width = 15)
     private String workNo;
 
     /**
      * 职务，关联职务表
      */
-    @Excel(name = "职务", width = 15)
+//    @Excel(name = "职务", width = 15)
     @Dict(dictTable ="sys_position",dicText = "name",dicCode = "code")
     private String post;
 
     /**
      * 座机号
      */
-    @Excel(name = "座机号", width = 15)
+//    @Excel(name = "座机号", width = 15)
     private String telephone;
 
     /**
@@ -165,13 +215,13 @@ public class SysUser implements Serializable {
     /**
      * 身份（0 普通成员 1 上级）
      */
-    @Excel(name="（1普通成员 2上级）",width = 15)
+//    @Excel(name="（1普通成员 2上级）",width = 15)
     private Integer userIdentity;
 
     /**
      * 负责部门
      */
-    @Excel(name="负责部门",width = 15,dictTable ="sys_depart",dicText = "depart_name",dicCode = "id")
+//    @Excel(name="负责部门",width = 15,dictTable ="sys_depart",dicText = "depart_name",dicCode = "id")
     @Dict(dictTable ="sys_depart",dicText = "depart_name",dicCode = "id")
     private String departIds;
 
